468a2aaeb536916c2d16765290bab9b5d31f2612
raguar <imraguar@gmail.com>
2021-07-19 19:28:40 +0530

    Update Commit

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-const button = document.getElementById('btn')
-const body = document.querySelector('body')
+const button = document.getElementById("btn");
+const body = document.querySelector("body");
+const hexaCode = document.querySelector(".hexa");
 
 const colorChange = () => {
+  const colorObj = {
+    red: Math.floor(Math.random() * 255),
+    green: Math.floor(Math.random() * 255),
+    blue: Math.floor(Math.random() * 255),
+  };
 
-    const colorObj = {
-        red : Math.floor(Math.random()*255),
-        green : Math.floor(Math.random()*255),
-        blue : Math.floor(Math.random()*255)
-    }
+  const rgbToHex = (r, g, b) =>
+    "#" +
+    [r, g, b]
+      .map((x) => {
+        const hex = x.toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+      })
+      .join("");
 
-    
-    let randomColor = `rgba(${colorObj.red},${colorObj.green},${colorObj.blue})`;
+  hexaCode.textContent = `${rgbToHex(
+    colorObj.red,
+    colorObj.green,
+    colorObj.blue
+  ).toUpperCase()}`;
 
-    
-    body.style.background = randomColor
-}
+  let randomColor = `rgba(${colorObj.red},${colorObj.green},${colorObj.blue})`;
 
-button.addEventListener('click', colorChange)
+  body.style.background = randomColor;
+};
 
+button.addEventListener("click", colorChange);
